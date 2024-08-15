eb4f46552b666bab1d77f04f03a0bdd9a05c3724
PhysShell <nixos@nixos.sector.kz>
2024-08-15 08:35:25 +0000

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -11,21 +11,22 @@
         go = pkgs.go_1_21; # Или любая другая подходящая версия Go
         dendrite = pkgs.buildGoModule rec {
           pname = "dendrite";
-          version = "v0.15.1"; # Обновите до последней версии Dendrite
+          version = "v0.13.7"; # Обновите до последней версии Dendrite
           src = pkgs.fetchFromGitHub {
             owner = "matrix-org";
             repo = "dendrite";
             rev = version;
-            sha256 = "sha256-0q6mscfs4qk42f9qikidyld6sxnn0prj02r0ls0s8mx97brx1ah3"; # Обновите с правильным SHA256
+            sha256 = "sha256-A6rQ8zqpV6SBpiALIPMF1nZtGvUtzoiTE2Rioh3T1WA="; # Обновите с правильным SHA256
           };
-          vendorSha256 = "sha256-0vqsn1z12amr26qhky1kf13nca77y25wzbbk6ik1k49ashyf7drj"; # Обновите с правильным SHA256
+          vendorHash = "sha256-ByRCI4MuU8/ilbeNNOXSsTlBVHL5MkxLHItEGeGC9MQ="; # Обновите с правильным SHA256
           doCheck = false; # Отключаем тесты на данный момент
           buildPhase = ''
-            make install
+            go build -o bin/ ./cmd/...
+	     # make install
           '';
           installPhase = ''
             mkdir -p $out/bin
-            cp -r ./bin/* $out/bin
+            cp -r bin/* $out/bin
           '';
         };
         configFile = pkgs.writeText "dendrite.yaml" ''
@@ -33,14 +34,35 @@
         '';
       in
       {
-        devShells.${system}.default = pkgs.mkShell {
-          buildInputs = [ go dendrite ];
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ go dendrite pkgs.postgresql ];
           shellHook = ''
             export PATH=$PATH:$(pwd)/bin
+	    export PGDATA=$PWD/postgres_data
+	    export PGHOST=$PWD/postgres
+	    export PGPORT=5432
+	    #export PGUSER=dendrite
+ 	    #export PGPASSWORD=dendritepass
+            if [ ! -d $PGDATA ]; then
+		initdb --auth=trust --no-locale --encoding=UTF8
+		echo "listen_addresses='127.0.0.1'" >> $PGDATA/postgresql.conf
+		echo "unix_socket_directories='$PGHOST'" >> $PGDATA/postgresql.conf		
+	    fi
+
+	    mkdir -p $PGHOST
+	    pg_ctl start -D $PGDATA -l postgres.log -o "-c unix_socket_directories='$PGHOST'"
+           # sleep 3  # Дать время PostgreSQL для запуска
+           # if ! psql -lqt | cut -d \| -f 1 | grep -qw dendrite; then
+		#createuser -s dendrite
+		#createdb -O dendrite dendrite
+	    #fi
           '';
         };
-        packages.${system}.default = dendrite;
-        apps.${system}.dendrite = {
+        # after run for now it needs to create yser manually like so
+        #createuser -s dendriteuser --pwprompt
+	#createdb -O dendriteuser ddb
+        packages.default = dendrite;
+        apps.default = {
           type = "app";
           program = "${dendrite}/bin/dendrite";
           # Пример конфигурации, настройте по необходимости
